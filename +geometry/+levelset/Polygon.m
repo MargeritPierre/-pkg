ead55d9b3bc56976a15efafc50133f362881ce50
Pierre Margerit <margeritpierre@gmail.com>
2021-02-01 18:20:16 +0100

    edede

--- +geometry/+levelset/Polygon.m
+++ +geometry/+levelset/Polygon.m
@@ -2,13 +2,14 @@ classdef Polygon < pkg.geometry.levelset.LevelSet
 % Level set representing a Polygon
 
 methods
-    function this = Polygon(points)
+    function this = Polygon(points,edgFcn)
     % Class construtor
+        if nargin<2 ; edgFcn = 'segments' ; end
         this = this@pkg.geometry.levelset.LevelSet() ;
         this.Function = @(p)pkg.geometry.distance.toPolygon(p,points) ;
         this.BoundingBox = [min(points,[],1) ; max(points,[],1)] ;
         this.Kinks = points ;
-        this.EdgeFcns = pkg.geometry.levelset.polylineEdgeFunctions(points([1:end,1],:)) ;
+        this.EdgeFcns = pkg.geometry.levelset.polylineEdgeFunctions(points([1:end,1],:),edgFcn) ;
     end
 end
     
--- +geometry/+levelset/Polyline.m
+++ +geometry/+levelset/Polyline.m
@@ -2,13 +2,14 @@ classdef Polyline < pkg.geometry.levelset.LevelSet
 % Level set representing a Polyline
 
 methods
-    function this = Polyline(points)
+    function this = Polyline(points,edgFcn)
     % Class construtor
+        if nargin<2 ; edgFcn = 'segments' ; end
         this = this@pkg.geometry.levelset.LevelSet() ;
         this.Function = @(p)pkg.geometry.distance.toPolyline(p,points) ;
         this.BoundingBox = [min(points,[],1) ; max(points,[],1)] ;
         this.Kinks = points ;
-        this.EdgeFcns = pkg.geometry.levelset.polylineEdgeFunctions(points) ;
+        this.EdgeFcns = pkg.geometry.levelset.polylineEdgeFunctions(points,edgFcn) ;
     end
 end
     
